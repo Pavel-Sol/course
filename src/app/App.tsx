4fef24015f61741adfce0855e650dcea0769d438
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-10-05 16:44:30 +0400

    created navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,18 @@
-import React, { Suspense } from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import React from "react";
 import "./styles/index.scss";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
