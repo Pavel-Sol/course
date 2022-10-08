7a7b2b8a9e36b56b725fda863a1b7729a745131c
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-10-08 12:43:44 +0400

    babel, extract-plugin

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -4,7 +4,11 @@ import { useTranslation } from "react-i18next";
 const AboutPage = () => {
   const { t } = useTranslation("about");
 
-  return <div>{t("О сайте")}</div>;
+  return (
+    <div>
+      {t("О сайте")} {t("О сайте какой")}
+    </div>
+  );
 };
 
 export default AboutPage;
